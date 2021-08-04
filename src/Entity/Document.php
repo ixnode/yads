28527417a90585415aa581b178626a0545c9d74c
Björn Hempel <bjoern@hempel.li>
2021-08-05 00:25:11 +0200

    #14; Add document_type.allowed_attributes check to POST document

--- src/Entity/Document.php
+++ src/Entity/Document.php
@@ -3,52 +3,33 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
-use DateTimeInterface;
 use Doctrine\ORM\Mapping as ORM;
+use App\Validator\DocumentValidator;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Class Document
  *
  * @author Björn Hempel <bjoern@hempel.li>
- * @version 1.0 (2021-07-31)
+ * @version 1.0 (2021-08-04)
  * @package App\Entity
  */
 #[ApiResource]
 #[ORM\Table(name: 'document')]
 #[ORM\Index(columns: ["document_type_id"], name: 'IDX_D8698A7661232A4F')]
-#[ORM\Entity]
-class Document
+#[ORM\Entity, ORM\HasLifecycleCallbacks]
+#[Assert\Callback([DocumentValidator::class, "validate"])]
+class Document extends BaseEntity
 {
-    #[ORM\Column(name: 'id', type: 'integer', nullable: false)]
-    #[ORM\Id]
-    #[ORM\GeneratedValue(strategy: 'IDENTITY')]
-    private int $id;
-
     /**
      * @var array[]
      */
     #[ORM\Column(name: 'data', type: 'json', length: 0, nullable: false)]
     private array $data;
 
-    #[ORM\Column(name: 'created_at', type: 'datetime', nullable: false)]
-    private DateTimeInterface $createdAt;
-
-    #[ORM\Column(name: 'updated_at', type: 'datetime', nullable: false)]
-    private DateTimeInterface $updatedAt;
-
     #[ORM\ManyToOne(targetEntity: DocumentType::class)]
     #[ORM\JoinColumn(name: 'document_type_id', referencedColumnName: 'id')]
     private DocumentType $documentType;
-
-    /**
-     * Returns the id of this entity.
-     *
-     * @return int
-     */
-    public function getId(): int
-    {
-        return $this->id;
-    }
 
     /**
      * Returns data values of this entity as array.
@@ -69,52 +50,6 @@ class Document
     public function setData(array $data): self
     {
         $this->data = $data;
-
-        return $this;
-    }
-
-    /**
-     * Returns the created at date of this entity.
-     *
-     * @return DateTimeInterface
-     */
-    public function getCreatedAt(): DateTimeInterface
-    {
-        return $this->createdAt;
-    }
-
-    /**
-     * Sets the created at date of this entity.
-     *
-     * @param DateTimeInterface $createdAt
-     * @return $this
-     */
-    public function setCreatedAt(DateTimeInterface $createdAt): self
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
-    }
-
-    /**
-     * Returns the updated at date of this entity.
-     *
-     * @return DateTimeInterface
-     */
-    public function getUpdatedAt(): DateTimeInterface
-    {
-        return $this->updatedAt;
-    }
-
-    /**
-     * Sets the updated at date of this entity.
-     *
-     * @param DateTimeInterface $updatedAt
-     * @return $this
-     */
-    public function setUpdatedAt(DateTimeInterface $updatedAt): self
-    {
-        $this->updatedAt = $updatedAt;
 
         return $this;
     }
